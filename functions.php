fb0c6b3024ebd6b03916118c40b328353d114838
Sovanny Huy <sovhu774@student.liu.se>
2019-10-16 07:59:49 +0200

    style first version done

---
+++ functions.php
@@ -0,0 +1,68 @@
+<?php
+/*------------------------------------*\
+	External Modules/Files
+\*------------------------------------*/
+
+// Load any external files you have here
+
+/*------------------------------------*\
+	Theme Support
+\*------------------------------------*/
+
+if (function_exists('add_theme_support'))
+{
+    // Add Thumbnail Theme Support
+    add_image_size('large', 700, '', true); // Large Thumbnail
+    add_image_size('medium', 250, '', true); // Medium Thumbnail
+    add_image_size('small', 120, '', true); // Small Thumbnail
+    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+}
+
+/*------------------------------------*\
+	Functions
+\*------------------------------------*/
+
+// Load HTML5 Blank scripts (header.php)
+function hpvico_header_scripts()
+{
+    if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
+        wp_enqueue_script('hpvicoscripts', get_template_directory_uri() . '/js/scripts.js', array(), '1.0.0'); // Custom scripts
+    }
+}
+
+
+// Load HTML5 Blank styles
+function hpvico_styles()
+{
+    wp_enqueue_style('hpvico', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    //wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), '4.3.1', 'all');
+}
+
+function remove_texteditor(){
+    remove_post_type_support('page', 'editor');
+}
+
+
+/*------------------------------------*\
+	Actions + Filters + ShortCodes
+\*------------------------------------*/
+
+// Add Actions
+add_action('init', 'hpvico_header_scripts'); // Add Custom Scripts to wp_head
+add_action('wp_enqueue_scripts', 'hpvico_styles'); // Add Theme Stylesheet
+add_action('admin_init', 'remove_texteditor');
+
+// Remove Actions
+remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
+remove_action('wp_head', 'feed_links', 2); // Display the links to the general feeds: Post and Comment Feed
+remove_action('wp_head', 'rsd_link'); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action('wp_head', 'wlwmanifest_link'); // Display the link to the Windows Live Writer manifest file.
+remove_action('wp_head', 'index_rel_link'); // Index link
+remove_action('wp_head', 'parent_post_rel_link', 10, 0); // Prev link
+remove_action('wp_head', 'start_post_rel_link', 10, 0); // Start link
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // Display relational links for the posts adjacent to the current post.
+remove_action('wp_head', 'wp_generator'); // Display the XHTML generator that is generated on the wp_head hook, WP version
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'rel_canonical');
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+/* Template name: Main Page */
+?>
+<?php get_header(); ?>
+
+<main class="main" role="main">
+	<section class="section hero">
+		<div class="hero-wrap">
+			<h1><?php the_field('headline_text'); ?></h1>
+			<h3 class="top-line--blue"><?php the_field('subheading') ?></h3>
+			<a class="cta cta--filled">
+				<?php the_field('contact_button_text') ?>
+			</a>
+		</div>
+		<div class="logo-side">
+			<?php include_once("img/hpvico_icons/svg/hpvico.svg") ?>
+		</div>
+	</section>
+
+	<section class="section ">
+
+		<?php
+		$itemCount = 0;
+		if (have_rows('product_feature_repeater')) :
+			while (have_rows('product_feature_repeater')) :
+				$itemCount++;
+				the_row();
+				?>
+				<?php 
+				if ($itemCount == 1) : ?>
+				<div class="items">
+					<div class="logo-arrow">
+						<?php include("img/hpvico_icons/svg/hpvico-arrow.svg") ?>
+					</div>
+				<?php endif; ?>
+
+				<div class="items__item">
+					<h3> <?php the_sub_field('title'); ?> </h3>
+					<p> <?php the_sub_field('text'); ?> </p>
+				</div>
+
+				<?php
+				if ($itemCount == 2) :
+					$itemCount = 0;
+				?>
+					</div>
+				<?php endif; ?>
+		<?php	
+			endwhile;
+		else :
+		// no rows found
+		endif;
+		?>
+	</section>
+
+	<section class="section bg-blue-dark">
+		<div class="text-content text-white">
+			<h2 class="bottom-line--white"> <?php the_field('about_title'); ?> </h2>
+			<p class="text-weight-lit"> <?php the_field('about_text'); ?> </p>
+		</div>
+	</section>
+
+	<section class="section form">
+		<div class="form-wrapper">
+		<div class="logo text-center">
+			<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+			<img src="<?php echo get_template_directory_uri(); ?>/img/hpvico_icons/png/HPVico_logga_250x207.png" alt="Logo" class="logo-img">
+		</div>
+			<h3 class="text-center"> <?php the_field('contact_us_text'); ?> </h3>
+			<form class="form__form">
+				<div class="form__input-name">Full name *</div>
+				<input class="form__input" type="text" name="fullname" ><br>
+				<div class="form__input-name">Company *</div>
+				<input class="form__input" type="text" name="company" ><br>
+				<div class="form__input-name">Phone *</div>
+				<input class="form__input" type="text" name="phone" ><br>
+				<div class="form__input-name">E-mail *</div>
+				<input class="form__input" type="text" name="e-mail" ><br>
+				<div class="form__input-name">Message *</div>
+				<textarea class="form__textarea" name="message"></textarea>
+				<div class="permission-wrapper">
+					<input class="form__checkbox" type="checkbox" name="gdpr-permission" value="checked"> 
+					<p class="text-discrete"> <?php the_field('permissions_checkbox_text'); ?> </p>
+				</div>
+				<div class="submit-wrapper">
+					<input class="form__submit cta" type="submit" name="submit" value="SUBMIT">
+				</div>
+			</form>
+		
+		</div>
+	</section>
+	<?php edit_post_link(); ?>
+</main>
+
+<?php get_footer(); ?>
